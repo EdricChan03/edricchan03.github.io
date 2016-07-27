17521c0ff8362d2a0415fe95d378a029f7c05e6c
Edric Chan <edric.chan.1997@gmail.com>
2016-07-27 15:56:13 +0800

    Add END

--- angularJS/404.js
+++ angularJS/404.js
@@ -1,7 +1,4 @@
 var app = angular.module('error', ['ngMaterial']);
 app.controller('ErrorController', function($scope) {
-    $scope.mainUrl = 'https://chan4077.github.io';
-    $scope.back = function() {
-        window.history.back();
-    };
+    
 });
