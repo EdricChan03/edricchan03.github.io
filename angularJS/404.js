7c1ca936cfd35b2d250c5129428aa176d186611b
Edric Chan <edric.chan.1997@gmail.com>
2016-08-14 22:04:57 +0800

    Test debug

--- angularJS/404.js
+++ angularJS/404.js
@@ -1,4 +1,8 @@
 var app = angular.module('error', ['ngMaterial']);
 app.controller('ErrorController', function($scope) {
-    
+    $scope.load = function() {
+      $scope.debug = console.log('Successfully loaded!');
+      return $scope.debug;
+      
+    };
 });
