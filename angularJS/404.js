773c57205cdda5822d76878c3da48132cdf8d910
Edric Chan <edric.chan.1997@gmail.com>
2016-08-14 22:29:09 +0800

    Wrong Spelling... :scream:

--- angularJS/404.js
+++ angularJS/404.js
@@ -3,13 +3,13 @@ app.controller('ErrorController', function($scope) {
     $scope.main = function() {
       window.location.href = "https://chan4077.github.io";
       console.log('Redirecting to main...');
-    }
+    };
     $scope.about = function() {
       window.location.href = "https://chan4077.github.io/about#angular";
       console.log('Redirecting To Site...');
-    }
+    };
     $scope.commit = function() {
-      window.loation.href = "https://github.com/Chan4077/chan4077.github.io/commits/master";
+      window.location.href = "https://github.com/Chan4077/chan4077.github.io/commits/master";
       console.log('Redirecting to Commits...');
     };
 });
