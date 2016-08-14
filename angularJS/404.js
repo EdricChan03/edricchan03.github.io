e99956f9b3520a866db83e995c8c2fa894ddeb7f
Edric Chan <edric.chan.1997@gmail.com>
2016-08-14 22:23:32 +0800

    Add text

--- angularJS/404.js
+++ angularJS/404.js
@@ -1,8 +1,15 @@
 var app = angular.module('error', ['ngMaterial']);
 app.controller('ErrorController', function($scope) {
-    $scope.load = function() {
-      $scope.debug = console.log('Successfully loaded!');
-      return $scope.debug;
-      
+    $scope.main = function() {
+      window.location.href = "https://chan4077.github.io";
+      console.log('Redirecting to main...');
+    }
+    $scope.about = function() {
+      window.location.href = "https://chan4077.github.io/about#angular";
+      console.log('Redirecting To Site...');
+    }
+    $scope.commit = function() {
+      window.loation.href = "https://github.com/Chan4077/chan4077.github.io/commits/master";
+      console.log('Redirecting to Commits...');
     };
 });
