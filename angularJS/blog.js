5d86e614fa708b59d79cc6181f9dcb666b589a76
Edric Chan <edric.chan.1997@gmail.com>
2016-08-29 11:29:14 +0800

    Change

--- angularJS/blog.js
+++ angularJS/blog.js
@@ -6,11 +6,13 @@ blog.controller('PostController', function($scope) {
             "Author":'Edric Chan', 
             "Job":'Author Of The Site', 
             "Headline":'Test', 
-            "Article":'The titles of Washed Out\'s breakthrough song and the first single from Paracosm share the two most important words in Ernest Greene\'s musical language: feel it. It\'s a simple request, as well...'},
+            "Article":'The titles of Washed Out\'s breakthrough song and the first single from Paracosm share the two most important words in Ernest Greene\'s musical language: feel it. It\'s a simple request, as well...'
+        },
         {
             "Author":'John Lim', 
             "Job":'UX Designer', 
             "Headline":'Note', 
-            "Article":'This card was made by me!'}
+            "Article":'This card was made by me!'
+        }
     ];
 });
