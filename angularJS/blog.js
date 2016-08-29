cad127ac233eb38e070a6506a5fcb4e45dea8e81
Edric Chan <edric.chan.1997@gmail.com>
2016-08-29 08:24:06 +0800

    Remove ""

--- angularJS/blog.js
+++ angularJS/blog.js
@@ -2,11 +2,7 @@ var blog = angular.module('blogApp', ['ngMaterial']);
 blog.controller('PostController', function($scope) {
     $scope.profile = '';
     $scope.posts = [
-        {
-            "author" : "Edric Chan",
-            "job" : "Author Of The Site",
-            "headline" : "Test",
-            "article" : "The titles of Washed Out\'s breakthrough song and the first single from Paracosm share the two most important words in Ernest Greene\'s musical language: feel it. It\'s a simple request, as well..."
-        }
+        {author:'Edric Chan', job:'Author Of The Site', headline:'Test', article:'The titles of Washed Out\'s breakthrough song and the first single from Paracosm share the two most important words in Ernest Greene\'s musical language: feel it. It\'s a simple request, as well...'},
+        {author:'John Lim', job:'UX Designer', headline:'Note', article:'This card was made by me!'}
     ];
 });
