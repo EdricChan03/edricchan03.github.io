80bae692cf38c00d4a53d805562d08b51010fd87
Edric Chan <edric.chan.1997@gmail.com>
2016-09-06 17:23:59 +0800

    Add whats_new_tmpl.html

--- angular/app.js
+++ angular/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('testApp', ['ngMaterial']);
-app.controller('MainController', function($scope, $mdSidenav) {
+app.controller('MainController', function ($scope, $mdSidenav, $mdDialog) {
     $scope.openLeftMenu = function () {
         $mdSidenav('left').toggle();
     };
@@ -26,24 +26,44 @@ app.controller('MainController', function($scope, $mdSidenav) {
     // More menu
     var originatorEv;
 
-    $scope.openMenu = function($mdOpenMenu, ev) {
-      originatorEv = ev;
-      $mdOpenMenu(ev);
+    $scope.openMenu = function ($mdOpenMenu, ev) {
+        originatorEv = ev;
+        $mdOpenMenu(ev);
     };
     // Initialize switches
     $scope.switch = {
-        form_debug : false,
+        form_debug: false,
     };
+    // $mdDialog (start)
+    $scope.whats_new = function (ev) {
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: 'whats_new_tmpl.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+        });
+    };
+    // $mdDialog (end)
+    function DialogController($scope, $mdDialog) {
+        $scope.hide = function () {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        };
+    };
+
 });
 
 app.controller('SideNavController', function ($scope) {
     $scope.links = [
-        {url: 'https://chan4077.github.io', icon: 'home', color_icon: 'black'},
-        {url: 'https://chan4077.github.io/about', icon: 'account_box', color_icon: 'black'},
-        {url: 'https://chan4077.github.io/blog', icon: '', color_icon: 'black'},
-        {url: 'https://chan4077.github.io/media', icon: 'group', color_icon: 'black'},
-        {url: 'https://chan4077.github.io/notes', icon: 'insert_drive_file', color_icon: 'black'},
-        {url: 'https://chan4077.github.io/preferences', icon: 'settings', color_icon: 'black'},
-        {url: 'https://chan4077.github.io/angular', icon: '', color_icon: 'black'}
+        { url: 'https://chan4077.github.io', icon: 'home', color_icon: 'black' },
+        { url: 'https://chan4077.github.io/about', icon: 'account_box', color_icon: 'black' },
+        { url: 'https://chan4077.github.io/blog', icon: '', color_icon: 'black' },
+        { url: 'https://chan4077.github.io/media', icon: 'group', color_icon: 'black' },
+        { url: 'https://chan4077.github.io/notes', icon: 'insert_drive_file', color_icon: 'black' },
+        { url: 'https://chan4077.github.io/preferences', icon: 'settings', color_icon: 'black' },
+        { url: 'https://chan4077.github.io/angular', icon: '', color_icon: 'black' }
     ]
 });
