91ce1babbd8823977d2ed00bcfb002ab3fe4fb64
Edric Chan <edric.chan.1997@gmail.com>
2016-09-16 22:10:09 +0800

    Add more

--- angular/app.js
+++ angular/app.js
@@ -36,7 +36,24 @@ app.controller('MainController', function ($scope, $mdSidenav, $mdDialog, $mdToa
         right: true
     };
     $scope.toastPosition = angular.extend({}, last);
+    $scope.getToastPosition = function () {
+        sanitizePosition();
 
+        return Object.keys($scope.toastPosition)
+            .filter(function (pos) { return $scope.toastPosition[pos]; })
+            .join(' ');
+    };
+
+    function sanitizePosition() {
+        var current = $scope.toastPosition;
+
+        if (current.bottom && last.top) current.top = false;
+        if (current.top && last.bottom) current.bottom = false;
+        if (current.right && last.left) current.left = false;
+        if (current.left && last.right) current.right = false;
+
+        last = angular.extend({}, current);
+    }
     $scope.formSubmit = function () {
         var pinTo = $scope.toastPosition();
         $mdToast.show(
