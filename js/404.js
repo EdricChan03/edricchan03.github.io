75825317ab87acfed955c56892f72d2f0e14f0c3
Edric Chan <edric.chan.1997@gmail.com>
2016-10-12 15:30:47 +0800

    Change to Go Back

--- js/404.js
+++ js/404.js
@@ -33,33 +33,3 @@ app.controller('MainController', function ($scope, $mdSidenav) {
     $mdSidenav('left').toggle();
   };
 }
-);
-/*
-*  Sidenav Controller
-*/
-app.controller('SideNavController', function ($scope, $mdSidenav) {
-    console.info('SideNavController successfully loaded!');
-
-    $scope.top = [
-        { url: 'https://chan4077.github.io', title: 'Main', icon: 'home' },
-        { url: 'https://chan4077.github.io/about', title: 'About Me', icon: 'account-box' },
-        { url: 'https://chan4077.github.io/blog', title: 'Blog', icon: 'blogger' },
-        { url: 'https://chan4077.github.io/media', title: 'Social Media', icon: 'google-plus' },
-        { url: 'https://chan4077.github.io/angular', title: 'Angular', icon: 'angular' },
-    ];
-    $scope.other = [
-        { url: 'https://github.com/Chan4077/chan4077.github.io', title: 'Project Page', icon: 'github-circle' },
-        { url: 'https://chan4077.github.io/preferences', title: 'Preferences', icon: 'settings' },
-        { url: '', title: 'Submit Feedback', icon: 'message-alert' },
-        { url: '', title: 'Get Help', icon: 'help-circle' },
-    ];
-    $scope.made = [
-        { url: 'https://materialdesignicons.com', title: 'MaterialDesignIcons', icon: 'vector-square' },
-        { url: 'https://material.angularjs.org/latest', title: 'Angular Material', icon: 'angular' },
-        { url: 'https://angularjs.org', title: 'Angular', icon: 'angular' },
-        { url: 'https://pages.github.com', title: 'Github Pages', icon: 'github-circle' }
-    ]
-    $scope.openLeftMenu = function () {
-        $mdSidenav('left').toggle();
-    }
-});
