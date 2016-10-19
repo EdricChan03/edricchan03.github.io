acafe852c40ce674f38bce5b403af0958ebe052f
Edric Chan <edric.chan.1997@gmail.com>
2016-10-19 16:52:08 +0800

    Add Feedback form

--- js/help.js
+++ js/help.js
@@ -4,13 +4,45 @@ app.config(function ($mdIconProvider, $mdToastProvider) {
     $mdIconProvider.defaultIconSet('/img/mdi.svg');
     console.info('Successfully initialized!');
 });
-app.controller('HelpController', function($scope) {
+app.controller('HelpController', function ($scope, $mdDialog) {
     console.info('Loaded HelpController!');
+$scope.sendFeedback = function(ev) {
+    $mdDialog.show({
+      controller: FeedbackController,
+      templateUrl: '/templates/feedbackhelp.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose: true
+    })
+    .then(function(answer) {
+      $scope.status = 'Your answer was: "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+
+app.controller('SearchController', function ($scope) {
+    console.info('Loaded SearchController!');
 });
+// Sidenav
+app.controller('SideNavController', function ($scope, $mdSidenav) {
+    console.info('SideNavController successfully loaded!');
 
-app.controller('SearchController', function($scope) {
-
-})
-
+    $scope.top = [
+        { url: 'https://chan4077.github.io', title: 'Main', icon: 'home' },
+        { url: 'https://chan4077.github.io/about', title: 'About Me', icon: 'account-box' },
+        { url: 'https://chan4077.github.io/blog', title: 'Blog', icon: 'blogger' },
+        { url: 'https://chan4077.github.io/media', title: 'Social Media', icon: 'google-plus' },
+        { url: 'https://chan4077.github.io/angular', title: 'Angular', icon: 'angular' },
+    ];
+    $scope.other = [
+        { url: 'https://github.com/Chan4077/chan4077.github.io', title: 'Project Page', icon: 'github-circle' },
+        { url: 'https://chan4077.github.io/preferences', title: 'Preferences', icon: 'settings' },
+        { url: 'https://chan4077.github.io/help', title: 'Get Help', icon: 'help-circle' },
+    ];
+    $scope.openLeftMenu = function () {
+        $mdSidenav('left').toggle();
+    }
+});
 // Directives
 // Come from beta site
