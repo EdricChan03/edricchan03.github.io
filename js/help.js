6cc3468192a3b3d0c95c09635c299a859199da7a
Edric Chan <edric.chan.1997@gmail.com>
2016-10-21 18:27:48 +0800

    Add ngAnimate, ngAria, ngMessages

--- js/help.js
+++ js/help.js
@@ -1,4 +1,4 @@
-var app = angular.module('HelpApp', ['ngMaterial']);
+var app = angular.module('HelpApp', ['ngMaterial', 'ngMessages', 'ngAnimate', 'ngAria']);
 // Config
 app.config(function ($mdIconProvider) {
     $mdIconProvider.defaultIconSet('/img/mdi.svg');
