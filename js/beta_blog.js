8647ae48fbd7459323d70056f6e3f7ad8e949d11
Edric Chan <edric.chan.1997@gmail.com>
2016-10-28 20:04:52 +0800

    Add another post

--- js/beta_blog.js
+++ js/beta_blog.js
@@ -1,15 +1,109 @@
 var app = angular.module('BlogApp', ['ngAnimate', 'ngAria', 'ngMaterial', 'ngMessages']);
 
-app.config(function($mdIconProvider) {
+app.config(function ($mdIconProvider) {
     $mdIconProvider
-    .defaultIconSet('/img/mdi.svg')
-    .icon('website-logo', '/img/website.svg');
+        .defaultIconSet('/img/mdi.svg')
+        .icon('website-logo', '/img/website.svg');
 
     console.info('Successfully initialized!');
 });
-app.controller('MainController', function($scope) {
+app.controller('MainController', function ($scope) {
+    $scope.refreshPage = function () {
+        window.location.reload(true);
+    };
+    // More menu
+    var originatorEv;
+
+    $scope.openMenu = function ($mdOpenMenu, ev) {
+        originatorEv = ev;
+        $mdOpenMenu(ev);
+    };
+    $scope.settings = function (ev) {
+        $mdDialog.show({
+            controller: SettingsController,
+            templateUrl: '/templates/settings_tmpl.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+        });
+    };
+    $scope.sendFeedback = function (ev) {
+        $mdDialog.show({
+            controller: FeedbackController,
+            templateUrl: '/templates/feedbackhelp.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+        });
+    };
+    function FeedbackController($scope, $mdDialog, $mdToast) {
+        $scope.hide = function () {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        };
+        $scope.help = function () {
+            window.location.href = "https://chan4077.github.io/help/about_feedback.html";
+            console.info('Redirecting to Help site...');
+        }
+        $scope.oriFeedback = {
+            name: "",
+            email: "",
+            sendFeedback: ""
+        };
+        // Declare a variable called oriFeedback
+        var oriFeedback = angular.copy($scope.oriFeedback);
+
+        // On initial, set feedback to original
+        $scope.feedback = angular.copy(oriFeedback);
+
+        // Reset form
+        $scope.reset = function () {
+            $scope.feedback = angular.copy(oriFeedback);
+            // Reset form
+            $scope.feedbackhelp.$setPristine();
+            // Reset validation errors
+            $scope.feedbackhelp.$setUntouched();
+        };
+        $scope.submitForm = function () {
+            $mdDialog.cancel();
+            $mdToast.show(
+                $mdToast.simple()
+                    .textContent('You submitted the form!')
+                    .position('bottom left')
+                    .hideDelay(3000)
+            );
+        };
+    };
+    function SettingsController($scope, $mdDialog) {
+        $scope.hide = function () {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        };
+        $scope.help = function () {
+            window.location.href = "https://chan4077.github.io/help";
+            console.info('Redirecting to Help site...');
+        }
+        $scope.general_settings = {
+            proBar: true,
+            enableSound: true
+        };
+        $scope.developer_settings = {
+            consoleLog: false,
+            showExtraOptions: false
+        };
+        $scope.extra_settings = {
+            showCredits: true,
+            enableOffline: true
+        };
+    };
+    // $mdDialog (end)
     $scope.cards = [
-        { avatar: '/img/logo.svg', name: 'Edric Chan', job: 'Owner', imagePath: '/img/angular_1.jpeg', imageAlt: 'Nice Flowers', postTitle: 'First Post', content: 'Welcome! This is some test post that will see if this works!', fullPost: '', halfSize: true, fixedSize: false},
-        { avatar: '', name: 'John Sim', job: 'Co-Author of Blog', imagePath: '', imageAlt: '', postTitle: 'Lorem ipsum dolor sit amet', content: 'Lorem ipsum dolor sit amet', fullPost: '', halfSize: false, fixedSize: false}
-    ]
+        { avatar: '/img/logo.svg', name: 'Edric Chan', job: 'Owner', imagePath: '/img/angular_1.jpeg', imageAlt: 'Nice Flowers', postTitle: 'First Post', content: 'Welcome! This is some test post that will see if this works!', fullPost: '', halfSize: true, fixedSize: false },
+        { avatar: '', name: 'John Sim', job: 'Co-Author of Blog', imagePath: '', imageAlt: '', postTitle: 'Lorem ipsum dolor sit amet', content: 'Lorem ipsum dolor sit amet', fullPost: '', halfSize: false, fixedSize: false },
+        { avatar: '', name: 'Joy', job: 'Designer', imagePath: '', imageAlt: '', postTitle: 'Another post!', content: 'This is my first post!', fullPost: '', halfSize: false, fixedSize: false }
+    ];
 });
