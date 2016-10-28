9d1fcf6a9bf5c8b1586d2a20dda562e97d0c1be5
Edric Chan <edric.chan.1997@gmail.com>
2016-10-28 19:57:51 +0800

    Add ng-class

--- js/beta_blog.js
+++ js/beta_blog.js
@@ -9,7 +9,7 @@ app.config(function($mdIconProvider) {
 });
 app.controller('MainController', function($scope) {
     $scope.cards = [
-        { avatar: '/img/logo.svg', name: 'Edric Chan', job: 'Owner', imagePath: '/img/angular_1.jpeg', imageAlt: 'Nice Flowers', postTitle: 'First Post', content: 'Welcome! This is some test post that will see if this works!', fullPost: ''},
-        { avatar: '', name: 'John Sim', job: 'Co-Author of Blog', imagePath: '', imageAlt: '', postTitle: 'Lorem ipsum dolor sit amet', content: 'Lorem ipsum dolor sit amet', fullPost: ''}
+        { avatar: '/img/logo.svg', name: 'Edric Chan', job: 'Owner', imagePath: '/img/angular_1.jpeg', imageAlt: 'Nice Flowers', postTitle: 'First Post', content: 'Welcome! This is some test post that will see if this works!', fullPost: '', halfSize: true, fixedSize: false},
+        { avatar: '', name: 'John Sim', job: 'Co-Author of Blog', imagePath: '', imageAlt: '', postTitle: 'Lorem ipsum dolor sit amet', content: 'Lorem ipsum dolor sit amet', fullPost: '', halfSize: false, fixedSize: false}
     ]
 });
