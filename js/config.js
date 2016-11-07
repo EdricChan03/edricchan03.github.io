5ed1993f4637a531828a9061c7a8440806adbc03
Edric Chan <edric.chan.1997@gmail.com>
2016-11-07 09:53:10 +0800

    Remove $log

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 angular.module('app.config', [])
-.config(function ($mdIconProvider, $mdToastProvider, $log) {
+.config(function ($mdIconProvider, $mdToastProvider) {
     $mdIconProvider
         .defaultIconSet('/img/mdi.svg')
         .icon('website-logo', '/img/website.svg');
-    $log.debug('Successfully initialized!');
+    console.info('Successfully initialized!');
 });
