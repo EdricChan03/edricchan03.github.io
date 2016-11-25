2a110712e25f64d6aacf632dd50b974b985e0f91
Edric Chan <edric.chan.1997@gmail.com>
2016-11-25 22:44:18 +0800

    fix(dependencies) - Move toolbar stuff to dependencies.js

--- js/dependencies.js
+++ js/dependencies.js
@@ -69,6 +69,125 @@ angular.module('app.directives', [])
                     $scope.title = '{{title}}';
                 }
                 console.log('url visited: ' + $location.path());
+                $scope.refreshPage = function () {
+                    window.location.reload(true);
+                };
+                // $mdDialog (start)
+                $scope.whats_new = function (ev) {
+                    $mdDialog.show({
+                        controller: WhatNewController,
+                        templateUrl: '/templates/whats_new_tmpl.html',
+                        parent: angular.element(document.body),
+                        targetEvent: ev,
+                        clickOutsideToClose: true,
+                    });
+                };
+                $scope.settings = function (ev) {
+                    $mdDialog.show({
+                        controller: SettingsController,
+                        templateUrl: '/templates/settings_tmpl.html',
+                        parent: angular.element(document.body),
+                        targetEvent: ev,
+                        clickOutsideToClose: true,
+                    });
+                };
+                $scope.sendFeedback = function (ev) {
+                    $mdDialog.show({
+                        controller: FeedbackController,
+                        templateUrl: '/templates/feedbackhelp.html',
+                        parent: angular.element(document.body),
+                        targetEvent: ev,
+                        clickOutsideToClose: true,
+                    });
+                };
+                // More menu
+                var originatorEv;
+
+                $scope.openMenu = function ($mdOpenMenu, ev) {
+                    originatorEv = ev;
+                    $mdOpenMenu(ev);
+                };
+                function WhatNewController($scope, $mdDialog, $mdToast) {
+                    $scope.hide = function () {
+                        $mdDialog.hide();
+                    };
+                    $scope.cancel = function () {
+                        $mdDialog.cancel();
+                    };
+                    $scope.help = function () {
+                        window.location.href = "https://chan4077.github.io/help";
+                        console.info('Redirecting to Help site...');
+                    }
+                };
+                function FeedbackController($scope, $mdDialog, $mdToast) {
+                    $scope.hide = function () {
+                        $mdDialog.hide();
+                    };
+                    $scope.cancel = function () {
+                        $mdDialog.cancel();
+                    };
+                    $scope.help = function () {
+                        window.location.href = "https://chan4077.github.io/help/about_feedback.html";
+                        console.info('Redirecting to Help site...');
+                    }
+                    $scope.oriFeedback = {
+                        name: "",
+                        email: "",
+                        sendFeedback: ""
+                    };
+                    // Declare a variable called oriFeedback
+                    var oriFeedback = angular.copy($scope.oriFeedback);
+
+                    // On initial, set feedback to original
+                    $scope.feedback = angular.copy(oriFeedback);
+
+                    // Reset form
+                    $scope.reset = function () {
+                        $scope.feedback = angular.copy(oriFeedback);
+                        // Reset form
+                        $scope.feedbackhelp.$setPristine();
+                        // Reset validation errors
+                        $scope.feedbackhelp.$setUntouched();
+                    };
+                    $scope.submitForm = function () {
+                        $mdDialog.cancel();
+                        $mdToast.show(
+                            $mdToast.simple()
+                                .textContent('You submitted the form!')
+                                .position('bottom left')
+                                .hideDelay(3000)
+                        );
+                    };
+                };
+                function SettingsController($scope, $mdDialog) {
+                    $scope.hide = function () {
+                        $mdDialog.hide();
+                    };
+                    $scope.cancel = function () {
+                        $mdDialog.cancel();
+                    };
+                    $scope.help = function () {
+                        window.location.href = "https://chan4077.github.io/help";
+                        console.info('Redirecting to Help site...');
+                    }
+                    $scope.general_settings = {
+                        proBar: true,
+                        enableSound: true
+                    };
+                    $scope.developer_settings = {
+                        consoleLog: false,
+                        showExtraOptions: false
+                    };
+                    $scope.extra_settings = {
+                        showCredits: true,
+                        enableOffline: true
+                    };
+                };
+                // $mdDialog (end)
+
+                $scope.about_site = function () {
+                    window.location.href = "https://chan4077.github.io/about#angular";
+                };
             }
         };
     })
--- js/test.js
+++ js/test.js
@@ -3,133 +3,13 @@ var app = angular.module('testApp', ['ngMaterial', 'ngMessages', 'ngAnimate', 'n
 app.controller('MainController', function ($scope, $mdSidenav, $mdDialog, $mdToast) {
     // Refresh page, direct from server
     console.info('MainController successfully loaded!');
-    $scope.refreshPage = function () {
-        window.location.reload(true);
-    };
     // Sidenav
     $scope.openLeftMenu = function () {
         $mdSidenav('left').toggle();
     };
-    // More menu
-    var originatorEv;
-
-    $scope.openMenu = function ($mdOpenMenu, ev) {
-        originatorEv = ev;
-        $mdOpenMenu(ev);
-    };
-
     // Initialize switches
     $scope.switch = {
         form_debug: false,
-    };
-    // $mdDialog (start)
-    $scope.whats_new = function (ev) {
-        $mdDialog.show({
-            controller: WhatNewController,
-            templateUrl: '/templates/whats_new_tmpl.html',
-            parent: angular.element(document.body),
-            targetEvent: ev,
-            clickOutsideToClose: true,
-        });
-    };
-    $scope.settings = function (ev) {
-        $mdDialog.show({
-            controller: SettingsController,
-            templateUrl: '/templates/settings_tmpl.html',
-            parent: angular.element(document.body),
-            targetEvent: ev,
-            clickOutsideToClose: true,
-        });
-    };
-    $scope.sendFeedback = function (ev) {
-        $mdDialog.show({
-            controller: FeedbackController,
-            templateUrl: '/templates/feedbackhelp.html',
-            parent: angular.element(document.body),
-            targetEvent: ev,
-            clickOutsideToClose: true,
-        });
-    };
-    function WhatNewController($scope, $mdDialog, $mdToast) {
-        $scope.hide = function () {
-            $mdDialog.hide();
-        };
-        $scope.cancel = function () {
-            $mdDialog.cancel();
-        };
-        $scope.help = function () {
-            window.location.href = "https://chan4077.github.io/help";
-            console.info('Redirecting to Help site...');
-        }
-    };
-    function FeedbackController($scope, $mdDialog, $mdToast) {
-        $scope.hide = function () {
-            $mdDialog.hide();
-        };
-        $scope.cancel = function () {
-            $mdDialog.cancel();
-        };
-        $scope.help = function () {
-            window.location.href = "https://chan4077.github.io/help/about_feedback.html";
-            console.info('Redirecting to Help site...');
-        }
-        $scope.oriFeedback = {
-            name: "",
-            email: "",
-            sendFeedback: ""
-        };
-        // Declare a variable called oriFeedback
-        var oriFeedback = angular.copy($scope.oriFeedback);
-
-        // On initial, set feedback to original
-        $scope.feedback = angular.copy(oriFeedback);
-
-        // Reset form
-        $scope.reset = function () {
-            $scope.feedback = angular.copy(oriFeedback);
-            // Reset form
-            $scope.feedbackhelp.$setPristine();
-            // Reset validation errors
-            $scope.feedbackhelp.$setUntouched();
-        };
-        $scope.submitForm = function () {
-            $mdDialog.cancel();
-            $mdToast.show(
-                $mdToast.simple()
-                    .textContent('You submitted the form!')
-                    .position('bottom left')
-                    .hideDelay(3000)
-            );
-        };
-    };
-    function SettingsController($scope, $mdDialog) {
-        $scope.hide = function () {
-            $mdDialog.hide();
-        };
-        $scope.cancel = function () {
-            $mdDialog.cancel();
-        };
-        $scope.help = function () {
-            window.location.href = "https://chan4077.github.io/help";
-            console.info('Redirecting to Help site...');
-        }
-        $scope.general_settings = {
-            proBar: true,
-            enableSound: true
-        };
-        $scope.developer_settings = {
-            consoleLog: false,
-            showExtraOptions: false
-        };
-        $scope.extra_settings = {
-            showCredits: true,
-            enableOffline: true
-        };
-    };
-    // $mdDialog (end)
-
-    $scope.about_site = function () {
-        window.location.href = "https://chan4077.github.io/about#angular";
     };
     $scope.fabitems = [
         { name: 'Facebook', icon: 'facebook-box' },
