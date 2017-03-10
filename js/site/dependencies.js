b10e8e16c13619284ae847d88bcbbc0e595785c7
Edric Chan <edric.chan.1997@gmail.com>
2017-03-10 17:15:49 +0800

    feat(dependencies) - Change to $mdToast for feedback

--- js/site/dependencies.js
+++ js/site/dependencies.js
@@ -103,124 +103,129 @@ angular.module('app.directives', [])
                         clickOutsideToClose: true,
                     });
                 };
-                $scope.sendFeedback = function (ev) {
-                    $mdDialog.show({
-                        controller: FeedbackController,
-                        templateUrl: '/templates/feedbackhelp.html',
-                        parent: angular.element(document.body),
-                        targetEvent: ev,
-                        clickOutsideToClose: true,
-                    });
-                };
-                // What's New
-                function WhatNewController($scope, $mdDialog, $mdToast) {
-                    // Hide button
-                    $scope.hide = function () {
-                        // Hides $mdDialog
-                        $mdDialog.hide();
-                    };
-                    // Cancel button
-                    $scope.cancel = function () {
-                        // Cancels $mdDialog
-                        $mdDialog.cancel();
-                    };
-                    // Help button
-                    $scope.help = function () {
-                        // Redirect to help site
-                        window.location.href = "https://chan4077.github.io/help";
-                        console.info('Redirecting to Help site...');
-                    }
-                    // More Info button
-                    $scope.moreInfo = function () {
-                        // Note that this is percent-encoded
-                        // More information: https://en.wikipedia.org/wiki/Percent-encoding
-                        window.location.href = "https://github.com/Chan4077/chan4077.github.io/wiki/What%27s-New";
-                    }
-                };
-                // Feedback
-                function FeedbackController($scope, $mdDialog, $mdToast) {
-                    $scope.hide = function () {
-                        $mdDialog.hide();
-                    };
-                    $scope.cancel = function () {
-                        $mdDialog.cancel();
-                        $mdToast.show(
-                            $mdToast.simple()
-                                .textContent('Feedback cancelled')
-                                .position('bottom left')
-                                .hideDelay(3000)
-                        );
-                    };
-                    $scope.help = function () {
-                        window.location.href = "https://chan4077.github.io/help/about_feedback.html";
-                        console.info('Redirecting to Help site...');
-                    }
-                    // $scope.oriFeedback = {
-                    //     name: "",
-                    //     email: "",
-                    //     sendFeedback: ""
-                    // };
-                    // // Declare a variable called oriFeedback
-                    // var oriFeedback = angular.copy($scope.oriFeedback);
+                $scope.sendFeedback = function () {
+                    // Head to Github -> Create new issue
+                    var toast = $mdToast.simple()
+                        .textContent('Send feedback here: ')
+                        .action('GO TO GITHUB')
+                        .highlightAction(true)
+                        .position(pinTo);
 
-                    // // On initial, set feedback to original
-                    // $scope.feedback = angular.copy(oriFeedback);
-
-                    // // Reset form
-                    // $scope.reset = function () {
-                    //     $scope.feedback = angular.copy(oriFeedback);
-                    //     // Reset form
-                    //     $scope.feedbackhelp.$setPristine();
-                    //     // Reset validation errors
-                    //     $scope.feedbackhelp.$setUntouched();
-                    // };
-                    $scope.postOnGithub = function () {
-                        $mdDialog.cancel();
-                        console.debug('Redirecting to Github...');
-                        window.location.href = "https://github.com/Chan4077/chan4077.github.io/issues/new";
+                    $mdToast.show(toast).then(function (response) {
+                        if (response == 'ok') {
+                            alert('Redirecting to Github...');
+                            window.location.href = 'https://github.com/Chan4077/chan4077.github.io/issues/new';
+                        }
                     };
-                };
-            // Settings
-            function SettingsController($scope, $mdDialog) {
-            $scope.hide = function () {
-                $mdDialog.hide();
-            };
-            $scope.cancel = function () {
-                $mdDialog.cancel();
-            };
-            $scope.help = function () {
-                window.location.href = "https://chan4077.github.io/help";
-                console.info('Redirecting to Help site...');
-            }
-            $scope.general_settings = {
-                proBar: true,
-                enableSound: true
-            };
-            $scope.developer_settings = {
-                consoleLog: false,
-                showExtraOptions: false
-            };
-            $scope.extra_settings = {
-                showCredits: true,
-                enableOffline: true
-            };
-            // $mdDialog (end)
+                    // What's New
+                    function WhatNewController($scope, $mdDialog, $mdToast) {
+                        // Hide button
+                        $scope.hide = function () {
+                            // Hides $mdDialog
+                            $mdDialog.hide();
+                        };
+                        // Cancel button
+                        $scope.cancel = function () {
+                            // Cancels $mdDialog
+                            $mdDialog.cancel();
+                        };
+                        // Help button
+                        $scope.help = function () {
+                            // Redirect to help site
+                            window.location.href = "https://chan4077.github.io/help";
+                            console.info('Redirecting to Help site...');
+                        }
+                        // More Info button
+                        $scope.moreInfo = function () {
+                            // Note that this is percent-encoded
+                            // More information: https://en.wikipedia.org/wiki/Percent-encoding
+                            window.location.href = "https://github.com/Chan4077/chan4077.github.io/wiki/What%27s-New";
+                        }
+                    };
+                    // Feedback
+                    function FeedbackController($scope, $mdDialog, $mdToast) {
+                        $scope.hide = function () {
+                            $mdDialog.hide();
+                        };
+                        $scope.cancel = function () {
+                            $mdDialog.cancel();
+                            $mdToast.show(
+                                $mdToast.simple()
+                                    .textContent('Feedback cancelled')
+                                    .position('bottom left')
+                                    .hideDelay(3000)
+                            );
+                        };
+                        $scope.help = function () {
+                            window.location.href = "https://chan4077.github.io/help/about_feedback.html";
+                            console.info('Redirecting to Help site...');
+                        }
+                        // $scope.oriFeedback = {
+                        //     name: "",
+                        //     email: "",
+                        //     sendFeedback: ""
+                        // };
+                        // // Declare a variable called oriFeedback
+                        // var oriFeedback = angular.copy($scope.oriFeedback);
 
-            $scope.about_site = function () {
-                window.location.href = "https://chan4077.github.io/about#angular";
+                        // // On initial, set feedback to original
+                        // $scope.feedback = angular.copy(oriFeedback);
+
+                        // // Reset form
+                        // $scope.reset = function () {
+                        //     $scope.feedback = angular.copy(oriFeedback);
+                        //     // Reset form
+                        //     $scope.feedbackhelp.$setPristine();
+                        //     // Reset validation errors
+                        //     $scope.feedbackhelp.$setUntouched();
+                        // };
+                        $scope.postOnGithub = function () {
+                            $mdDialog.cancel();
+                            console.debug('Redirecting to Github...');
+                            window.location.href = "https://github.com/Chan4077/chan4077.github.io/issues/new";
+                        };
+                    };
+                    // Settings
+                    function SettingsController($scope, $mdDialog) {
+                        $scope.hide = function () {
+                            $mdDialog.hide();
+                        };
+                        $scope.cancel = function () {
+                            $mdDialog.cancel();
+                        };
+                        $scope.help = function () {
+                            window.location.href = "https://chan4077.github.io/help";
+                            console.info('Redirecting to Help site...');
+                        }
+                        $scope.general_settings = {
+                            proBar: true,
+                            enableSound: true
+                        };
+                        $scope.developer_settings = {
+                            consoleLog: false,
+                            showExtraOptions: false
+                        };
+                        $scope.extra_settings = {
+                            showCredits: true,
+                            enableOffline: true
+                        };
+                        // $mdDialog (end)
+
+                        $scope.about_site = function () {
+                            window.location.href = "https://chan4077.github.io/about#angular";
+                        };
+                    };
+                }
             };
-        };
-    }
-        };
-    })
+        })
     // Speed Dial
     .directive('siteSpeedDial', function () {
-    return {
-        restrict: 'E',
-        templateUrl: '/templates/speeddial.html',
-        transclude: true
-    };
-})
+        return {
+            restrict: 'E',
+            templateUrl: '/templates/speeddial.html',
+            transclude: true
+        };
+    })
     // Controllers
     .controller('SideNavController', function ($scope, $mdSidenav) {
 
