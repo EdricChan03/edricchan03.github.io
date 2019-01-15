0a0bd34fc222b1b4dbf92bc84cecf198eac5b60f
Edric Chan <edric.chan.1997@gmail.com>
2019-01-15 23:14:10 +0800

    Add script for postinstall

---
+++ postinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Current directory/file permissions:"
+
+ls -hAFl
+
+echo "Copying node_modules to _sass directory..."
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  # Running in GitHub Action Docker container
+  sudo cp -Rf node_modules _sass/
+else
+  cp -Rf node_modules _sass/
+fi
