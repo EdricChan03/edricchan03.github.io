7bb6f7159c2013216ae1a5cc386e86489cf2ae7d
Chan4077 <Chan4077@users.noreply.github.com>
2019-04-08 01:37:18 +0000

    Deploy commit 8c615f710b798cc0bed0e065bc4a7ed4565e38ac
    Autodeployed using Build & Deploy to GitHub Pages in Build and deploy to GitHub Pages on push

--- js/site.js
+++ js/site.js
@@ -1,3 +1,37 @@
+/**
+ * Copies the `text` to the clipboard
+ * @param {string} text The text to copy
+ * @param {HTMLInputElement} inputEl The input element (if any)
+ * @param {boolean} logDebugMsgs Whether to log debugging messages
+ * @returns A `Promise` if the async Clipboard API is supported; or `true` if the `text` was successfully copied, or `false` otherwise
+ */
+function copyText(text, inputEl, logDebugMsgs) {
+  if (navigator.clipboard && navigator.clipboard.writeText) {
+    if (typeof logDebugMsgs === 'boolean' && logDebugMsgs === true) {
+      console.debug('copyText: Using Async Clipboard API');
+    }
+    return navigator.clipboard.writeText(window.location.href);
+  } else if (document.execCommand) {
+    if (typeof logDebugMsgs === 'boolean' && logDebugMsgs === true) {
+      console.debug('copyText: Using Document.execCommand API');
+    }
+    if (inputEl) {
+      inputEl.select();
+      document.execCommand('copy');
+      return true;
+    } else {
+      const el = document.createElement('textarea');
+      el.value = text;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
 var activeListItemClass = 'mdc-list-item--activated';
 var drawer = mdc.drawer.MDCDrawer.attachTo(document.getElementById('app-drawer'));
 var topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(document.getElementById('app-bar'));
@@ -42,6 +76,46 @@ if (path === '/about') {
   document.getElementById('about-list-item').classList.toggle(activeListItemClass);
 } else if (path.indexOf('/blog/') > -1) {
   document.getElementById('blog-list-item').classList.toggle(activeListItemClass);
+  if (document.getElementById('share-dialog') && document.getElementById('share-button')) {
+    const shareDialog = mdc.dialog.MDCDialog.attachTo(document.getElementById('share-dialog'));
+    const shareDialogSuccessSnackbar = mdc.snackbar.MDCSnackbar.attachTo(document.getElementById('share-dialog-success-snackbar'));
+    const shareDialogFailureSnackbar = mdc.snackbar.MDCSnackbar.attachTo(document.getElementById('share-dialog-failure-snackbar'));
+  
+    document.getElementById('share-button').addEventListener('click', () => {
+      shareDialog.open();
+    })
+    if (document.querySelectorAll('.mdc-text-field')) {
+      document.querySelectorAll('.mdc-text-field').forEach((el) => {
+        mdc.textField.MDCTextField.attachTo(el);
+      })
+    }
+    document.getElementById('share-dialog-copy-link-button')
+      .addEventListener('click', () => {
+        const copyVal = copyText(window.location.href, document.getElementById('share-dialog-text-field-input').value);
+        Promise.resolve(copyVal).then((value) => {
+          if (typeof value === 'boolean') {
+            if (value === true) {
+              // Successfully copied to the clipboard!
+              console.log('Successfully copied to the clipboard!');
+              shareDialogSuccessSnackbar.open();
+            } else {
+              // Could not copy
+              console.error('An error occurred while attempting to copy to the clipboard');
+              shareDialogFailureSnackbar.open();
+            }
+          } else {
+            // Successfully copied to the clipboard!
+            console.log('Successfully copied to the clipboard!');
+            shareDialogSuccessSnackbar.open();
+          }
+        })
+        .catch((error) => {
+          // Could not copy
+          console.error('An error occurred while attempting to copy to the clipboard:', error);
+          shareDialogFailureSnackbar.open();
+        })
+      })
+  }
   if (path === '/blog/authors') {
     document.querySelectorAll('.follow-author-menu')
       .forEach((element) => {
