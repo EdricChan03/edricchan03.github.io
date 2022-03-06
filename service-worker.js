9be5a846a75145f3cebb77e77b368cff4c7984d2
EdricChan03 <EdricChan03@users.noreply.github.com>
2022-03-06 18:51:58 +0000

    Deploy commit 8cf9761a7779dccd7e47152f435f5ed8f04d5051
    Autodeployed using __EdricChan03_action-build-deploy-ghpages in Jekyll site CI

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,7 @@ workbox.routing.setDefaultHandler(workbox.strategies.networkFirst());
 
 // Let Workbox handle the precaching list.
 // Note: This will be populated by jekyll-workbox-plugin.
-workbox.precaching.precacheAndRoute([{"url":"/blog/2022/03/07/status-update.html","revision":"6c50cd69bb6c51aa32cb6ca05e83b735"},{"url":"/blog/2019/04/21/easy-calculus-differentiation-integration.html","revision":"a6db6cd74e27483e7deb7756f324e561"},{"url":"/blog/programming/2019/03/16/simplified-content-views.html","revision":"a6a713afc9e6c8572b1e53662857d547"},{"url":"/blog/2019/02/24/status-update.html","revision":"dc112dc7b8ae6bceab390be04c084c27"},{"url":"/til/stackoverflow/2019/02/06/stackoverflow-backtick.html","revision":"f120f0600eb6ca0ba100017155b522d7"}]);
+workbox.precaching.precacheAndRoute([{"url":"/blog/2022/03/07/status-update.html","revision":"f2bcd5a512b5e9932d0038f0b606e905"},{"url":"/blog/2019/04/21/easy-calculus-differentiation-integration.html","revision":"a6db6cd74e27483e7deb7756f324e561"},{"url":"/blog/programming/2019/03/16/simplified-content-views.html","revision":"a6a713afc9e6c8572b1e53662857d547"},{"url":"/blog/2019/02/24/status-update.html","revision":"dc112dc7b8ae6bceab390be04c084c27"},{"url":"/til/stackoverflow/2019/02/06/stackoverflow-backtick.html","revision":"f120f0600eb6ca0ba100017155b522d7"}]);
 
 // Routing for image files.
 workbox.routing.registerRoute(
